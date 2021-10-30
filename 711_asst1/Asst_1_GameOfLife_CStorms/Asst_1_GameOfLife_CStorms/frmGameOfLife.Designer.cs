3054f93a340a8b020385c9afc27ae40f4eb75219
Carl Storms <37883693+TheBIMsider@users.noreply.github.com>
2021-10-29 18:02:53 -0600

    711 A1

---
+++ 711_asst1/Asst_1_GameOfLife_CStorms/Asst_1_GameOfLife_CStorms/frmGameOfLife.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Asst_1_GameOfLife_CStorms
+{
+    partial class frmGameOfLife
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGameOfLife));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grpLastGridLoaction = new System.Windows.Forms.GroupBox();
+            this.txtStatusTextBox = new System.Windows.Forms.TextBox();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtGenertionsCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnResetAllCells = new System.Windows.Forms.Button();
+            this.btnSaveGrid = new System.Windows.Forms.Button();
+            this.btnLoadGrid = new System.Windows.Forms.Button();
+            this.grpGenerationGroupBox = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.grpLastGridLoaction.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.grpLastGridLoaction);
+            this.groupBox1.Controls.Add(this.btnQuit);
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Location = new System.Drawing.Point(810, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 770);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game Controls";
+            // 
+            // grpLastGridLoaction
+            // 
+            this.grpLastGridLoaction.Controls.Add(this.txtStatusTextBox);
+            this.grpLastGridLoaction.Location = new System.Drawing.Point(28, 360);
+            this.grpLastGridLoaction.Name = "grpLastGridLoaction";
+            this.grpLastGridLoaction.Size = new System.Drawing.Size(261, 79);
+            this.grpLastGridLoaction.TabIndex = 4;
+            this.grpLastGridLoaction.TabStop = false;
+            this.grpLastGridLoaction.Text = "Last Grid Location";
+            // 
+            // txtStatusTextBox
+            // 
+            this.txtStatusTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtStatusTextBox.Location = new System.Drawing.Point(9, 35);
+            this.txtStatusTextBox.Name = "txtStatusTextBox";
+            this.txtStatusTextBox.Size = new System.Drawing.Size(241, 20);
+            this.txtStatusTextBox.TabIndex = 3;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnQuit.Location = new System.Drawing.Point(191, 711);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(100, 35);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtGenertionsCount);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Controls.Add(this.btnNext);
+            this.groupBox4.Location = new System.Drawing.Point(26, 244);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(265, 81);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Single Step";
+            // 
+            // txtGenertionsCount
+            // 
+            this.txtGenertionsCount.Location = new System.Drawing.Point(132, 36);
+            this.txtGenertionsCount.Name = "txtGenertionsCount";
+            this.txtGenertionsCount.Size = new System.Drawing.Size(50, 20);
+            this.txtGenertionsCount.TabIndex = 2;
+            this.txtGenertionsCount.Text = "1";
+            this.txtGenertionsCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGenertionsCount.TextChanged += new System.EventHandler(this.txtGenertionsCount_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Generations";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(23, 28);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(100, 35);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnResetAllCells);
+            this.groupBox3.Controls.Add(this.btnSaveGrid);
+            this.groupBox3.Controls.Add(this.btnLoadGrid);
+            this.groupBox3.Location = new System.Drawing.Point(26, 33);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(265, 183);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "File Operations";
+            // 
+            // btnResetAllCells
+            // 
+            this.btnResetAllCells.Location = new System.Drawing.Point(23, 129);
+            this.btnResetAllCells.Name = "btnResetAllCells";
+            this.btnResetAllCells.Size = new System.Drawing.Size(100, 35);
+            this.btnResetAllCells.TabIndex = 1;
+            this.btnResetAllCells.Text = "Reset All Cells";
+            this.btnResetAllCells.UseVisualStyleBackColor = true;
+            this.btnResetAllCells.Click += new System.EventHandler(this.btnResetAllCells_Click);
+            // 
+            // btnSaveGrid
+            // 
+            this.btnSaveGrid.Location = new System.Drawing.Point(23, 80);
+            this.btnSaveGrid.Name = "btnSaveGrid";
+            this.btnSaveGrid.Size = new System.Drawing.Size(100, 35);
+            this.btnSaveGrid.TabIndex = 1;
+            this.btnSaveGrid.Text = "Save Grid";
+            this.btnSaveGrid.UseVisualStyleBackColor = true;
+            this.btnSaveGrid.Click += new System.EventHandler(this.btnSaveGrid_Click);
+            // 
+            // btnLoadGrid
+            // 
+            this.btnLoadGrid.Location = new System.Drawing.Point(23, 30);
+            this.btnLoadGrid.Name = "btnLoadGrid";
+            this.btnLoadGrid.Size = new System.Drawing.Size(100, 35);
+            this.btnLoadGrid.TabIndex = 0;
+            this.btnLoadGrid.Text = "Load Grid";
+            this.btnLoadGrid.UseVisualStyleBackColor = true;
+            this.btnLoadGrid.Click += new System.EventHandler(this.btnLoadGrid_Click);
+            // 
+            // grpGenerationGroupBox
+            // 
+            this.grpGenerationGroupBox.BackColor = System.Drawing.SystemColors.Control;
+            this.grpGenerationGroupBox.Location = new System.Drawing.Point(15, 10);
+            this.grpGenerationGroupBox.Name = "grpGenerationGroupBox";
+            this.grpGenerationGroupBox.Size = new System.Drawing.Size(770, 770);
+            this.grpGenerationGroupBox.TabIndex = 1;
+            this.grpGenerationGroupBox.TabStop = false;
+            this.grpGenerationGroupBox.Text = "Current Generation ";
+            // 
+            // frmGameOfLife
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 796);
+            this.Controls.Add(this.grpGenerationGroupBox);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmGameOfLife";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game of Life";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmGameOfLife_FormClosing);
+            this.Load += new System.EventHandler(this.MakeGrid);
+            this.groupBox1.ResumeLayout(false);
+            this.grpLastGridLoaction.ResumeLayout(false);
+            this.grpLastGridLoaction.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox grpGenerationGroupBox;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnResetAllCells;
+        private System.Windows.Forms.Button btnSaveGrid;
+        private System.Windows.Forms.Button btnLoadGrid;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox txtGenertionsCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStatusTextBox;
+        private System.Windows.Forms.GroupBox grpLastGridLoaction;
+    }
+}
+
---
+++ 711_asst1/Asst_1_GameOfLife_CStorms/Asst_1_GameOfLife_CStorms/frmGameOfLife.cs
@@ -0,0 +1,344 @@
+﻿/*****************************************************************************************************************************
+*  
+*  Author:     Carl G Storms - All Rights Reserved
+*              July 2019
+*
+*  Purpose:    Class Assigment - University of Calgary
+*                                Continuing Education
+*                                ICT 711 - Computer Programming Level 2
+*
+*  Description:    
+*
+*                  Expected Behaviour - Current Generation Group Box will display the current Game of Life world in grid
+*                                       form (default is 30 rows x 30 columns, the defaul can by changed by the via constants
+*                                     - Load Grid Button will load a file that contains the coordinates of live
+*                                       (green) creatures, the defaul file type will be .gol
+*                                     - Save Grid Button will save the current generation of alive creatures (green cells)
+*                                       out do a file, the defaul file type will be .gol
+*                                     - Reset All Cells Button will set all kill all creatures and set the Game of Life world
+*                                       (grid) back to the start of time (all cells will be white) The bool array will also
+*                                       all be set back to false (dead / white)
+*                                     - Next Button will caclulate the next generation of creatures
+*                                     - Generations Text box will allow user input for calculate n generations
+*                                     - Last Grid Location Text box give a readout for the row and column of the last cell
+*                                       clicked on              
+*
+*****************************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;  //added IO system program
+
+namespace Asst_1_GameOfLife_CStorms
+{
+
+    public partial class frmGameOfLife : Form
+    {
+        private const int ROW_GRID_SIZE = 30; //row constant
+        private const int COLUMN_GRID_SIZE = 30; //column constant
+        private Label[,] theGrid = new Label[ROW_GRID_SIZE, COLUMN_GRID_SIZE];
+        private bool[,] currentGeneration = new bool[ROW_GRID_SIZE, COLUMN_GRID_SIZE];
+
+
+        public frmGameOfLife()
+        {
+            InitializeComponent();
+        }
+
+
+        private void birthCreature(int row, int column) // method to turn cell green and changes bool to true
+        {
+            theGrid[row, column].BackColor = Color.LimeGreen;
+            currentGeneration[row, column] = true;
+        }
+
+
+        private void killCreature(int row, int column) // method to turn cell white and changes bool to falce
+        {
+            theGrid[row, column].BackColor = Color.White;
+            currentGeneration[row, column] = false;
+        }
+
+
+        private void labelClickEventHandler(object sender, EventArgs e)
+        {
+            Label gs = (Label)sender; // gs is short for grid squares
+
+            int row = (int)gs.Tag / 1000;
+            int column = (int)gs.Tag % 1000;
+
+            if (gs.BackColor == Color.White) // if dead (white) make alive (green)
+            {
+                gs.BackColor = Color.LimeGreen;
+                currentGeneration[row, column] = true;
+            }
+
+            else // if alive (green) make dead (white)
+            {
+                currentGeneration[row, column] = false;
+                gs.BackColor = Color.White;
+            }
+
+            txtStatusTextBox.Text = $"Last location selected was row {row}, column {column}";
+
+        }
+
+
+        private void MakeGrid(object sender, EventArgs e)
+        {
+            Label gs;  // gs is short for grid squares
+
+            for (int row = 0; row < ROW_GRID_SIZE; row ++)
+            {
+                for (int column = 0; column < COLUMN_GRID_SIZE; column++)
+                {
+                    gs = new System.Windows.Forms.Label();
+                    gs.Tag = (row + 1) * 1000 + (column + 1); //adds cell location to tag
+                    gs.Location = new System.Drawing.Point(25 + 24 * column, 25 + 24 * row);
+                    gs.Size = new System.Drawing.Size(25, 25);
+                    gs.BackColor = System.Drawing.Color.White;
+                    gs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    gs.Click += new System.EventHandler(labelClickEventHandler);
+                    grpGenerationGroupBox.Controls.Add(gs);
+                    theGrid[row, column] = gs;
+                }
+            }
+        }
+
+
+        private void loadGeneration(StreamReader file)
+        {
+            // reads and imports coordinate file
+            string lineOfInput;
+            char[] delimiters = { ' ', ',', '\t' };
+            string[] tokens;
+            int row = 0;
+            int column = 0;
+
+            while (!file.EndOfStream)
+            {
+                lineOfInput = file.ReadLine();
+                tokens = lineOfInput.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                row = int.Parse(tokens[0]);
+                column = int.Parse(tokens[1]);
+
+                birthCreature(row, column); //makes cells green based on imported coordinates
+            }
+        }
+
+
+        private void btnLoadGrid_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogIn = new OpenFileDialog();    // reference to the open file dialog
+
+            dialogIn.Title = "Load Grid File";
+            dialogIn.Filter = "All Files|*.*|Game of Life Files (*.gol)|*.gol"; // set .gol as default file type with option for all files
+            dialogIn.FilterIndex = 2;
+
+            DialogResult result = dialogIn.ShowDialog();
+
+            try
+            {
+                StreamReader myDataFile = new StreamReader(dialogIn.FileName);
+                loadGeneration(myDataFile);
+
+                myDataFile.Close();
+            }
+          
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message); //output message if an exception is caught by the try-catch
+            }
+
+        }
+
+
+        private void btnSaveGrid_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogOut = new SaveFileDialog();       // reference to the SaveFileDialog object.
+            dialogOut.Title = "Save Grid Loacton";
+            dialogOut.Filter = "All Files|*.*|Game of Life Files (*.gol)|*.gol";  // set .gol as default file type with option for all files
+            dialogOut.FilterIndex = 2;
+
+            DialogResult result = dialogOut.ShowDialog();
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            try
+            {
+                // create the file of just the alive (green) cells, the file is just row, 
+                // column coordinates for the green cells of current generation
+                StreamWriter myOutputFile = new StreamWriter(dialogOut.FileName);
+
+                for (int row = 0; row < ROW_GRID_SIZE; row++)
+                {
+                    for (int column = 0; column < COLUMN_GRID_SIZE; column++)
+                    {
+                        if (currentGeneration[row, column] == true)
+                        {
+                            myOutputFile.WriteLine($"{row}, { column}");
+                        }
+                    }
+                }
+
+                myOutputFile.Close();
+
+                MessageBox.Show($"File '{dialogOut.FileName} written.");
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message); //output message if an exception is caught by the try-catch
+            }
+        }
+
+
+        private void btnResetAllCells_Click(object sender, EventArgs e)
+        {
+            var textboxes = grpLastGridLoaction.Controls.OfType<TextBox>();
+
+            foreach (TextBox tb in textboxes) // resets the Last Grid Location text box back to blank (empty)
+            {
+                tb.Text = "";
+            }
+
+            // resets all the cells back to white be killing any alive creatures (green cells) with the killCreature method
+            for (int row = 0; row < ROW_GRID_SIZE; row++)     
+            {
+                for (int column = 0; column < COLUMN_GRID_SIZE; column++)
+                {
+                    killCreature(row, column);
+                }
+            }
+        }
+
+
+        //// work in progress
+        //private void calculateNextGeneration()
+        //{
+        //    int row;
+        //    int column;
+        //    int[,] neighbourCounts = new int[ROW_GRID_SIZE, COLUMN_GRID_SIZE];
+
+        //    for (row = 0; row < ROW_GRID_SIZE; row++)
+        //    {
+        //        for (column = 0; column < COLUMN_GRID_SIZE; column++)
+        //        {
+        //            // need info from countTheNeighbours Method
+        //        }
+        //    }
+        //}
+
+
+        //// Work in Progress
+        //private void countTheNeigbours(int[,] numberOfNeighbours)
+        //{
+        //    int row;
+        //    int column;
+        //    int count = 0;
+
+        //    for (row = 0; row < ROW_GRID_SIZE; row++)
+        //    {
+        //        for (column = 0; column < COLUMN_GRID_SIZE; column++)
+        //        {
+        //            numberOfNeighbours[row, column] = - currentGeneration[row, column];
+        //            // need info from neighbourCount
+        //        }
+        //    }
+        //}
+
+
+        private void neighbourCount(int row, int column)
+        {
+            int count = 0;
+
+            if (currentGeneration[row - 1, column - 1] == true)
+                count++;
+            if (currentGeneration[row, column] == true)
+                count++;
+            if (currentGeneration[row - 1, column + 1] == true)
+                count++;
+            if (currentGeneration[row, column - 1] == true)
+                count++;
+            if (currentGeneration[row, column + 1] == true)
+                count++;
+            if (currentGeneration[row + 1, column - 1] == true)
+                count++;
+            if (currentGeneration[row + 1, column] == true)
+                count++;
+            if (currentGeneration[row + 1, column + 1] == true)
+                count++;
+
+            //return count;
+        }
+
+        //// Work in Progress
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+        //    bool gridChanged = false;
+
+        //    int generationCount = int.Parse(txtGenertionsCount.Text);
+
+        //    for (int i = 0; i < generationCount; i++)
+        //    {
+        //        gridChanged = calculateNextGeneration();
+        //        if (!gridChanged)
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    //display changes to UI somehow?
+
+        //    if (!gridChanged)
+        //    {
+        //        MessageBox.Show("The Game of Life world is now stable!");
+        //        btnNext.Enabled = false;
+        //    }
+        }
+
+
+         //Work in Progress
+        private void txtGenertionsCount_TextChanged(object sender, EventArgs e)
+        {
+            // this is where I get the input from the user for n generation
+            // this input will then be used to figure out the births and deaths for each of the n generations
+            // once the all gerations are calculated the final (last) generation will be displayed
+            // need to find the right way to display the final generation
+        }
+
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            // the Game of Life will close when the "Quit" button on is clicked, with a pop-up to confirm your choice
+            DialogResult quitDialog = MessageBox.Show("Do you really want to quit the Game of Life?",
+                                                      "Quit", MessageBoxButtons.YesNo);
+
+            if (quitDialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+            else if (quitDialog == DialogResult.No)
+            {
+                return;
+            }
+        }
+
+
+        private void frmGameOfLife_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit(); // the Game of Life will close when the red "X" button on the form is clicked
+        }
+    }
+}
